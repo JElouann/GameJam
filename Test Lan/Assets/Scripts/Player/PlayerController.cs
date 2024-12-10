bc8da794c2eafd91d86a9e260a5a54377d16bffd
Chorizo6969 <146953802+Chorizo6969@users.noreply.github.com>
2024-12-10 14:13:43 +0100

    vrai

--- Test Lan/Assets/Scripts/Player/PlayerController.cs
+++ Test Lan/Assets/Scripts/Player/PlayerController.cs
@@ -5,12 +5,10 @@ using Unity.Netcode;
 public class PlayerController : NetworkBehaviour
 {
     public float speed = 5f;
-    public float jumpForce = 5f;
     public float gravity = -9.81f;
 
     private CharacterController controller;
     private Vector3 velocity;
-    private bool isGrounded;
 
     private void Start()
     {
@@ -25,22 +23,11 @@ public class PlayerController : NetworkBehaviour
 
     private void Movement()
     {
-        isGrounded = controller.isGrounded;
-
-        if (isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * moveX + transform.forward * moveZ;
         controller.Move(move * speed * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
-        }
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
