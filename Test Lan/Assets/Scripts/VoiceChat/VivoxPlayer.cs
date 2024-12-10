c9fad719d760117a5ef2c68f7f47864b7f689edd
JElouann <elouann.jouenne@e-artsup.net>
2024-12-10 13:55:01 +0100

    ̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿

--- Test Lan/Assets/Scripts/VoiceChat/VivoxPlayer.cs
+++ Test Lan/Assets/Scripts/VoiceChat/VivoxPlayer.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using Unity.Services.Vivox;
 using UnityEngine;
 using Unity.Netcode;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
 
 public class VivoxPlayer : NetworkBehaviour
 {
@@ -23,11 +25,54 @@ public class VivoxPlayer : NetworkBehaviour
         if (IsLocalPlayer)
         {
             InitializeAsync();
+
+            VivoxService.Instance.LoggedIn += onLoggedIn;
+            VivoxService.Instance.LoggedOut += onLoggedOut;
+        }
+    }
+
+    private void Update()
+    {
+        if (IsIn3DChannel && IsLocalPlayer)
+        {
+            if ( Time.time > _nextPosUpdate)
+            {
+                updatePlayer3DPos();
+                _nextPosUpdate += 0.3f;
+            }
+        }
+        {
+            
         }
     }
 
     private async void InitializeAsync()
     {
+        await UnityServices.InitializeAsync();
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        await VivoxService.Instance.InitializeAsync();
+        Debug.Log("Vivox: Initialize successfull");
+    }
 
+    private void onLoggedIn()
+    {
+        if (VivoxService.Instance.IsLoggedIn)
+        {
+            Debug.Log("Vivox: Client" + _clientID + "Login successfull");
+        }
+        else
+        {
+            Debug.Log("Player: Cannot sign into Vivox, check your credentials and token settings");
+        }
+    }
+
+    private void onLoggedOut()
+    {
+        IsIn3DChannel = false;
+
+        VivoxService.Instance.LeaveAllChannelsAsync();
+        Debug.Log("Vivox: Left from all channels");
+        VivoxService.Instance.LogoutAsync();
+        Debug.Log("Vivox: Logged out from Vivox");
     }
 }
